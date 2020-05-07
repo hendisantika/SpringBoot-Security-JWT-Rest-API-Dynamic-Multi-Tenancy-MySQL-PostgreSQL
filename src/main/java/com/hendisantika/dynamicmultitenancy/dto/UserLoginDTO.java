dfef106910e7b7e0ebaec6f3d86d4a0d9bf44e60
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-08 05:39:32 +0700

    #6 - Add UserLoginDTO

---
+++ src/main/java/com/hendisantika/dynamicmultitenancy/dto/UserLoginDTO.java
@@ -0,0 +1,26 @@
+package com.hendisantika.dynamicmultitenancy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-jwt-rest-api-dynamic-multi-tenancy-mysql-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/05/20
+ * Time: 05.38
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginDTO implements Serializable {
+
+    private String userName;
+    private String password;
+    private Integer tenantOrClientId;
+}
