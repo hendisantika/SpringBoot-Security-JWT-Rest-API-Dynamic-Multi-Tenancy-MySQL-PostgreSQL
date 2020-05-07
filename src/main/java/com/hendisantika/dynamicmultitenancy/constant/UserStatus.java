bf037aed96c26097c1e02423b343d45fc36fb904
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-08 05:37:31 +0700

    #4 - Add UserStatus Enum

---
+++ src/main/java/com/hendisantika/dynamicmultitenancy/constant/UserStatus.java
@@ -0,0 +1,14 @@
+package com.hendisantika.dynamicmultitenancy.constant;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-jwt-rest-api-dynamic-multi-tenancy-mysql-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/05/20
+ * Time: 05.37
+ */
+public enum UserStatus {
+    ACTIVE, INACTIVE
+}
