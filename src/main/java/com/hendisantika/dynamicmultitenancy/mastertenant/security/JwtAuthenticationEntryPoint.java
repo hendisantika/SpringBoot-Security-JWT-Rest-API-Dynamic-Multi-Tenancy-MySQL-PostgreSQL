5cacc876f80bc6c97a449bf082f80040f10a1d8f
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-08 05:51:35 +0700

    #18 - Add JwtAuthenticationEntryPoint

---
+++ src/main/java/com/hendisantika/dynamicmultitenancy/mastertenant/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,32 @@
+package com.hendisantika.dynamicmultitenancy.mastertenant.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-jwt-rest-api-dynamic-multi-tenancy-mysql-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/05/20
+ * Time: 05.51
+ */
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+    private static final long serialVersionUID = -7858869558953243875L;
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
