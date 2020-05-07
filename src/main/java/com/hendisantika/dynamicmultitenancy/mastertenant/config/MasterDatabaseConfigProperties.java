5b53848369107ac3fcc81e2c7bd195596ce89151
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-08 05:46:38 +0700

    #13 - Add toString in MasterDatabaseConfigProperties

--- src/main/java/com/hendisantika/dynamicmultitenancy/mastertenant/config/MasterDatabaseConfigProperties.java
+++ src/main/java/com/hendisantika/dynamicmultitenancy/mastertenant/config/MasterDatabaseConfigProperties.java
@@ -25,4 +25,31 @@ public class MasterDatabaseConfigProperties {
     private long idleTimeout;
     private int minIdle;
     private String poolName;
+
+    //Initialization of HikariCP.
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("MasterDatabaseConfigProperties [url=");
+        builder.append(url);
+        builder.append(", username=");
+        builder.append(username);
+        builder.append(", password=");
+        builder.append(password);
+        builder.append(", driverClassName=");
+        builder.append(driverClassName);
+        builder.append(", connectionTimeout=");
+        builder.append(connectionTimeout);
+        builder.append(", maxPoolSize=");
+        builder.append(maxPoolSize);
+        builder.append(", idleTimeout=");
+        builder.append(idleTimeout);
+        builder.append(", minIdle=");
+        builder.append(minIdle);
+        builder.append(", poolName=");
+        builder.append(poolName);
+        builder.append("]");
+        return builder.toString();
+    }
+
 }
