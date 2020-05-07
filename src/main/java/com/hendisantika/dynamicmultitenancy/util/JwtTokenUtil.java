1ae09ee45ace62d5b9ae6b28e736b457e74ea3dc
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-08 05:41:47 +0700

    #8 - Add getUsernameFromToken, getAudienceFromToken, getExpirationDateFromToken, getClaimFromToken & getAllClaimsFromToken Method in JwtTokenUtil

--- src/main/java/com/hendisantika/dynamicmultitenancy/util/JwtTokenUtil.java
+++ src/main/java/com/hendisantika/dynamicmultitenancy/util/JwtTokenUtil.java
@@ -1,8 +1,13 @@
 package com.hendisantika.dynamicmultitenancy.util;
 
+import com.hendisantika.dynamicmultitenancy.constant.JWTConstants;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.function.Function;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +23,27 @@ public class JwtTokenUtil implements Serializable {
 
     private static final long serialVersionUID = -2550185165626007488L;
 
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public String getAudienceFromToken(String token) {
+        return getClaimFromToken(token, Claims::getAudience);
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(JWTConstants.SIGNING_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }
