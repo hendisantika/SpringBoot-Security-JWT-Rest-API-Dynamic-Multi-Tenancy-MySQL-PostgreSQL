083873cbc50cf6126022493ed4a923abd28053a7
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-08 05:40:18 +0700

    #7 - Add JwtTokenUtil

---
+++ src/main/java/com/hendisantika/dynamicmultitenancy/util/JwtTokenUtil.java
@@ -0,0 +1,21 @@
+package com.hendisantika.dynamicmultitenancy.util;
+
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-jwt-rest-api-dynamic-multi-tenancy-mysql-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/05/20
+ * Time: 05.40
+ */
+@Component
+public class JwtTokenUtil implements Serializable {
+
+    private static final long serialVersionUID = -2550185165626007488L;
+
+}
