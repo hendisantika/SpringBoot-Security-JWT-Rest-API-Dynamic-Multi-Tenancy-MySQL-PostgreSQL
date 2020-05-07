083e11fada719d7d0c5257f67f6bd840f77cb47a
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-08 06:09:56 +0700

    #39 - Add loadUserByUsername & getAuthority service in JwtUserDetailsService

--- src/main/java/com/hendisantika/dynamicmultitenancy/security/JwtUserDetailsService.java
+++ src/main/java/com/hendisantika/dynamicmultitenancy/security/JwtUserDetailsService.java
@@ -1,9 +1,16 @@
 package com.hendisantika.dynamicmultitenancy.security;
 
+import com.hendisantika.dynamicmultitenancy.tenant.entity.User;
 import com.hendisantika.dynamicmultitenancy.tenant.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +27,17 @@ public class JwtUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(userName);
+        if (null == user) {
+            throw new UsernameNotFoundException("Invalid user name or password.");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(),
+                getAuthority());
+    }
+
+    private List<SimpleGrantedAuthority> getAuthority() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+    }
 }
