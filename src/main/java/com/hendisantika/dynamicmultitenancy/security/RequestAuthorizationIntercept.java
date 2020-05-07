be0a00aa2a08b84177eee0990287aa6a4c57f14c
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-08 06:14:22 +0700

    #45 - Add checkPermission in RequestAuthorizationIntercept

--- src/main/java/com/hendisantika/dynamicmultitenancy/security/RequestAuthorizationIntercept.java
+++ src/main/java/com/hendisantika/dynamicmultitenancy/security/RequestAuthorizationIntercept.java
@@ -1,10 +1,18 @@
 package com.hendisantika.dynamicmultitenancy.security;
 
+import com.hendisantika.dynamicmultitenancy.mastertenant.config.DBContextHolder;
 import com.hendisantika.dynamicmultitenancy.mastertenant.service.MasterTenantService;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +33,19 @@ public class RequestAuthorizationIntercept {
     @Autowired
     private MasterTenantService masterTenantService;
 
+    @Around("@annotation(com.hendisantika.dynamicmultitenancy.security.RequestAuthorization)")
+    public Object checkPermission(ProceedingJoinPoint pjp) throws Throwable {
+        UserTenantInformation tenantInformation = applicationContext.getBean(UserTenantInformation.class);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        if (null == userDetails) {
+            throw new RuntimeException("Access is Denied. Please again login or  contact service provider");
+        }
+        Map<String, String> map = tenantInformation.getMap();
+        String tenantName = map.get(userDetails.getUsername());
+        if (tenantName != null && tenantName.equals(DBContextHolder.getCurrentDb())) {
+            return pjp.proceed();
+        }
+        throw new RuntimeException("Access is Denied. Please again login or contact service provider");
+    }
 }
