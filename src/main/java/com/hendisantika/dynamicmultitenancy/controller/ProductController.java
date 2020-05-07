866679c08d453b535c780d16e7eafbf18947ffcd
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-08 06:24:27 +0700

    #54 - Add getAllProduct API in ProductController

--- src/main/java/com/hendisantika/dynamicmultitenancy/controller/ProductController.java
+++ src/main/java/com/hendisantika/dynamicmultitenancy/controller/ProductController.java
@@ -1,9 +1,13 @@
 package com.hendisantika.dynamicmultitenancy.controller;
 
+import com.hendisantika.dynamicmultitenancy.security.RequestAuthorization;
 import com.hendisantika.dynamicmultitenancy.tenant.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +30,11 @@ public class ProductController implements Serializable {
 
     @Autowired
     private ProductService productService;
+
+    @RequestAuthorization
+    @GetMapping(value = "/all")
+    public ResponseEntity<Object> getAllProduct() {
+        LOGGER.info("getAllProduct() method call...");
+        return new ResponseEntity<>(productService.getAllProduct(), HttpStatus.OK);
+    }
 }
