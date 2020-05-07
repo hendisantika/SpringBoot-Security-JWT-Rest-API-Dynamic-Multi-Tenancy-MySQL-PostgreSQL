e7b6eb868e3f081178cf858d394decdf804b3fb4
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-08 05:42:30 +0700

    #9 - Add isTokenExpired, generateToken & doGenerateToken  Method in JwtTokenUtil

--- src/main/java/com/hendisantika/dynamicmultitenancy/util/JwtTokenUtil.java
+++ src/main/java/com/hendisantika/dynamicmultitenancy/util/JwtTokenUtil.java
@@ -3,9 +3,12 @@ package com.hendisantika.dynamicmultitenancy.util;
 import com.hendisantika.dynamicmultitenancy.constant.JWTConstants;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.function.Function;
 
@@ -45,5 +48,28 @@ public class JwtTokenUtil implements Serializable {
                 .setSigningKey(JWTConstants.SIGNING_KEY)
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    public String generateToken(String userName, String tenantOrClientId) {
+        return doGenerateToken(userName, tenantOrClientId);
+    }
+
+    private String doGenerateToken(String subject, String tenantOrClientId) {
+
+        Claims claims = Jwts.claims().setSubject(subject).setAudience(tenantOrClientId);
+        claims.put("scopes", Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")));
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuer("system")
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWTConstants.ACCESS_TOKEN_VALIDITY_SECONDS * 1000))
+                .signWith(SignatureAlgorithm.HS256, JWTConstants.SIGNING_KEY)
+                .compact();
     }
 }
