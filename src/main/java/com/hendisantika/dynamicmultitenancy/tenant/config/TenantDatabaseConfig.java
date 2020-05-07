098b815d7a19503df533bbdaf6f299a317e0932e
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-08 06:06:38 +0700

    #37 - Add entityManagerFactory Bean in TenantDatabaseConfig

--- src/main/java/com/hendisantika/dynamicmultitenancy/tenant/config/TenantDatabaseConfig.java
+++ src/main/java/com/hendisantika/dynamicmultitenancy/tenant/config/TenantDatabaseConfig.java
@@ -1,5 +1,7 @@
 package com.hendisantika.dynamicmultitenancy.tenant.config;
 
+import org.hibernate.MultiTenancyStrategy;
+import org.hibernate.cfg.Environment;
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
 import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -10,10 +12,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -65,5 +70,38 @@ public class TenantDatabaseConfig {
     @Bean(name = "currentTenantIdentifierResolver")
     public CurrentTenantIdentifierResolver currentTenantIdentifierResolver() {
         return new CurrentTenantIdentifierResolverImpl();
+    }
+
+    /**
+     * Creates the entity manager factory bean which is required to access the
+     * JPA functionalities provided by the JPA persistence provider, i.e.
+     * Hibernate in this case.
+     *
+     * @param connectionProvider
+     * @param tenantResolver
+     * @return
+     */
+    @Bean(name = "tenantEntityManagerFactory")
+    @ConditionalOnBean(name = "datasourceBasedMultitenantConnectionProvider")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            @Qualifier("datasourceBasedMultitenantConnectionProvider")
+                    MultiTenantConnectionProvider connectionProvider,
+            @Qualifier("currentTenantIdentifierResolver")
+                    CurrentTenantIdentifierResolver tenantResolver) {
+        LocalContainerEntityManagerFactoryBean emfBean = new LocalContainerEntityManagerFactoryBean();
+        //All tenant related entities, repositories and service classes must be scanned
+        emfBean.setPackagesToScan("com.amran.dynamic.multitenant");
+        emfBean.setJpaVendorAdapter(jpaVendorAdapter());
+        emfBean.setPersistenceUnitName("tenantdb-persistence-unit");
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(Environment.MULTI_TENANT, MultiTenancyStrategy.DATABASE);
+        properties.put(Environment.MULTI_TENANT_CONNECTION_PROVIDER, connectionProvider);
+        properties.put(Environment.MULTI_TENANT_IDENTIFIER_RESOLVER, tenantResolver);
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+        properties.put(Environment.SHOW_SQL, true);
+        properties.put(Environment.FORMAT_SQL, true);
+        properties.put(Environment.HBM2DDL_AUTO, "none");
+        emfBean.setJpaPropertyMap(properties);
+        return emfBean;
     }
 }
