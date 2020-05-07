3ef3ae183e701e703c20fb1729b41e5d27951ed2
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-08 06:03:18 +0700

    #31 - Add SpringBootServletInitializer in Main Class

--- src/main/java/com/hendisantika/dynamicmultitenancy/SpringbootSecurityJwtRestApiDynamicMultiTenancyMysqlPostgresqlApplication.java
+++ src/main/java/com/hendisantika/dynamicmultitenancy/SpringbootSecurityJwtRestApiDynamicMultiTenancyMysqlPostgresqlApplication.java
@@ -2,9 +2,16 @@ package com.hendisantika.dynamicmultitenancy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootSecurityJwtRestApiDynamicMultiTenancyMysqlPostgresqlApplication {
+public class SpringbootSecurityJwtRestApiDynamicMultiTenancyMysqlPostgresqlApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootSecurityJwtRestApiDynamicMultiTenancyMysqlPostgresqlApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootSecurityJwtRestApiDynamicMultiTenancyMysqlPostgresqlApplication.class, args);
