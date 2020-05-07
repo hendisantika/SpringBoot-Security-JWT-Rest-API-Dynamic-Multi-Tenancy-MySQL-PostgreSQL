998fda86ad8d073c04c06aa46b0d2f9e034c03ba
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-08 05:54:22 +0700

    #20 - Add override resolveCurrentTenantIdentifier & validateExistingCurrentSessions in CurrentTenantIdentifierResolverImpl

--- src/main/java/com/hendisantika/dynamicmultitenancy/tenant/config/CurrentTenantIdentifierResolverImpl.java
+++ src/main/java/com/hendisantika/dynamicmultitenancy/tenant/config/CurrentTenantIdentifierResolverImpl.java
@@ -1,5 +1,7 @@
 package com.hendisantika.dynamicmultitenancy.tenant.config;
 
+import com.hendisantika.dynamicmultitenancy.mastertenant.config.DBContextHolder;
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
 
 /**
@@ -14,4 +16,15 @@ import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
 public class CurrentTenantIdentifierResolverImpl implements CurrentTenantIdentifierResolver {
 
     private static final String DEFAULT_TENANT_ID = "client_tenant_1";
+
+    @Override
+    public String resolveCurrentTenantIdentifier() {
+        String tenant = DBContextHolder.getCurrentDb();
+        return StringUtils.isNotBlank(tenant) ? tenant : DEFAULT_TENANT_ID;
+    }
+
+    @Override
+    public boolean validateExistingCurrentSessions() {
+        return true;
+    }
 }
