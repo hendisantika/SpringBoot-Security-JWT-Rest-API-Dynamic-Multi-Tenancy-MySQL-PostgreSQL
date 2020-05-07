91745fb61ef76725a55d8be45ae47599e10009c5
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-08 06:23:21 +0700

    #52 - Add logoutFromApp API in LogoutController

--- src/main/java/com/hendisantika/dynamicmultitenancy/controller/LogoutController.java
+++ src/main/java/com/hendisantika/dynamicmultitenancy/controller/LogoutController.java
@@ -1,13 +1,19 @@
 package com.hendisantika.dynamicmultitenancy.controller;
 
+import com.hendisantika.dynamicmultitenancy.security.UserTenantInformation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.Serializable;
+import java.security.Principal;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +31,14 @@ public class LogoutController implements Serializable {
 
     @Autowired
     private ApplicationContext applicationContext;
+
+    @GetMapping(value = "/logout")
+    public ResponseEntity<?> logoutFromApp(Principal principal) {
+        LOGGER.info("AuthenticationController::logoutFromApp() method call..");
+        UserTenantInformation userCharityInfo = applicationContext.getBean(UserTenantInformation.class);
+        Map<String, String> map = userCharityInfo.getMap();
+        map.remove(principal.getName());
+        userCharityInfo.setMap(map);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
 }
